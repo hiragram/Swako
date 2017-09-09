f00ac91f2fc869caa9d624b35178843ace6cd215
Yuya Hirayama <y@yura.me>
2017-09-10 03:33:52 +0900

    wip

--- swifty_flavors.py
+++ swifty_flavors.py
@@ -1,6 +1,6 @@
 import re
 
-def typeName(property):
+def typeNameFromProperty(property):
     primitive = typeNameString(property.type)
     if primitive != None:
         return primitive
@@ -8,6 +8,14 @@ def typeName(property):
     if property.type == "array":
         elementName = typeNameString(property.itemType)
         return "Array<" + elementName + ">"
+
+def typeNameFromParameter(parameter):
+    primitive = typeNameString(parameter.type)
+
+    if primitive == None:
+        return "???????????????"
+
+    return primitive
 
 def typeNameString(typeName):
     if typeName == "integer":
@@ -18,6 +26,18 @@ def typeNameString(typeName):
         return "Bool"
     elif typeName == "array":
         return None
+    elif typeName == "file":
+        return "File is not supported"
+    elif typeName == None:
+        return None
     else:
+        print(typeName)
         m = re.search(r"#/definitions/(.*)", typeName)
         return m.group(1)
+
+def makeCamelCase(snakeCaseString):
+    return re.sub("_(.)",lambda x:x.group(1).upper(), snakeCaseString)
+
+def makePascalCase(snakeCaseString):
+    camelCase = makeCamelCase(snakeCaseString)
+    return camelCase[0].upper() + camelCase[1:]
